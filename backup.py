ae7901ff22449dd40ebd103ba7cc1750c82f6c57
Gülçin Nurəlizadə <116623821+Nuralizada@users.noreply.github.com>
2024-12-02 10:21:13 +0400

    Update backup.py

--- backup.py
+++ backup.py
@@ -17,7 +17,7 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
     st.session_state.user_id = None
 
-elif not st.session_state.authenticated:
+if not st.session_state.authenticated:
     st.title("Tətbiqə Giriş")
 
     # İstifadəçidən ID və parol tələb olunur
