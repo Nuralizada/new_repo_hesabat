4c793cd9b328b5e8429e08b0866a67a9810e9d30
Gülçin Nurəlizadə <116623821+Nuralizada@users.noreply.github.com>
2024-12-02 09:34:41 +0400

    Update backup.py

--- backup.py
+++ backup.py
@@ -33,7 +33,8 @@ if not st.session_state.authenticated:
         else:
             st.error("Yanlış istifadəçi ID və ya parol.")
 
-if st.session_state.authenticated:
+if st.session_state.page == "reports" and st.session_state.authenticated:
+
 
             
             import streamlit as st
